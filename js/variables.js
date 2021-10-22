f2d9a3f509afca511dac93dd9b774b2398ed975f
Adrian Santi <adrianesteban_88@hotmail.com>
2021-10-22 12:16:43 -0300

    javascriptClase1

---
+++ js/variables.js
@@ -0,0 +1,9 @@
+// entrada
+let nombreUsuario = prompt("ingresar nombre")
+
+// proceso
+let saludo = "Hola" + " " + nombreUsuario + " " + "Bienvenido a AS Clothing Store";
+
+// salida
+alert (saludo);
+console.log(saludo);
